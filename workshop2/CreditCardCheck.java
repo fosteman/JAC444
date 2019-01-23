edf381c825de3ac4cbecd44a0665aa104818fab0
HyunjiLee <hyunji.lee.tech@gmail.com>
2019-01-23 01:29:43 -0500

    Add functions for credit number valid check

--- workshop2/CreditCardCheck.java
+++ workshop2/CreditCardCheck.java
@@ -1,21 +1,37 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-public class CreditCardCheck {
+public class CreditCardCheck{
 	
 	//method 1
 	public static boolean checkCreditValid(long cardNum) {
+		int validNum = 0;
+		if(cardPrefix(cardNum)) {
+			if(numOfDigit(cardNum) >= 13 && numOfDigit(cardNum) <= 16) {
+				validNum = SecondDigitDoubleSum(cardNum) + oddSum(cardNum);
+				if(validNum %10 == 0) {
+					return true;
+				}else {
+					return false;
+				}
+			}	
+		}
 		return false;
 	}
 	
 	//method 2
-	public static int cardNumSum(long cardNum) {
+	public static int SecondDigitDoubleSum(long cardNum) {
 		int cardNumOfDigit = numOfDigit(cardNum);
-		int cardNumSum = 0, tenDivision = 1, i;
+		int secondDigitDouble = 0;
+		int cardNumSum = 0, i, j;
+		long tenDivision = 10;
 		
 		for(i = 0; i < cardNumOfDigit; i++) {
-			cardNumSum += ((cardNum % (tenDivision * 10)) - (cardNum % tenDivision)) / tenDivision;
-			tenDivision *= 10;
+			secondDigitDouble = (int)(2 * (((cardNum % (tenDivision * 10)) - (cardNum % tenDivision)) / tenDivision));
+			tenDivision *= 100;
+			i++;
+			
+			cardNumSum += cardNumSumCheck(secondDigitDouble);
 		}
 		
 		return cardNumSum;
@@ -36,8 +52,8 @@ public class CreditCardCheck {
 		int sum = 0;
 		
 		for(int i = 1; i < numOfDigit(cardNum) + 1; i++) {
-			double modularNum = Math.pow(10, i);
-			sum += (cardNum % (modularNum * 10)) - (cardNum % modularNum);
+			double modularNum = Math.pow(10, i) / 10;
+			sum += ((cardNum % (modularNum * 10)) - (cardNum % modularNum)) / modularNum;
 			i++;
 		}
 		
@@ -46,7 +62,15 @@ public class CreditCardCheck {
 	
 	//method 5
 	public static boolean cardPrefix(long cardNum) {
-		return false;
+		int cardDigitNum = numOfDigit(cardNum);
+		int prefix1 = (int)(cardNum / Math.pow(10, cardDigitNum - 1));
+		int prefix2 = (int)(cardNum / Math.pow(10, cardDigitNum - 2));
+		
+		if(prefix1 == 4 || prefix1 == 5 || prefix1 == 6 || prefix2 == 37) {
+			return true;
+		}else {
+			return false;
+		}
 	}
 	
 	//method 6
@@ -62,8 +86,16 @@ public class CreditCardCheck {
 	}
 	
 	//method 7
-	public static int firstKNumber(long cardNum) {
-		return 0;
+	public static long firstKNumber(long cardNum, int k) {
+		long product = 1;
+		
+		for(int i = 0; i < cardNum; i++)
+			product *= cardNum;
+		
+		while((int)(product / Math.pow(10, k)) != 0)
+			product = product / 10;
+		
+		return product;
 	}
 
 	public static void main(String[] args) {
@@ -75,7 +107,11 @@ public class CreditCardCheck {
 		System.out.print("  ");
 		creditCardNum = input.nextLong();
 		
-		System.out.print(oddSum(creditCardNum));
+		if(checkCreditValid(creditCardNum)) {
+			System.out.println(creditCardNum + " is valid");
+		}else {
+			System.out.println(creditCardNum + " is invalid");
+		}
 		
 		
 	}
