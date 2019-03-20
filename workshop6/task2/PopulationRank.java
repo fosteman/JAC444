d30316c8f46e17511dccfcb4e191c96bfee632a0
HyunjiLee <hyunji.lee.tech@gmail.com>
2019-03-19 20:25:18 -0400

    Add task2 functions

---
+++ workshop6/task2/PopulationRank.java
@@ -0,0 +1,76 @@
+package com.senecacollege.workshop6.task2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class PopulationRank {
+	String rank[];
+	String boyNum[];
+	String girlNum[];
+	String boyName[];
+	String girlName[];
+	
+	public PopulationRank() {
+		rank = new String[1000];
+		boyNum = new String[1000];
+		girlNum = new String[1000];
+		boyName = new String[1000];
+		girlName = new String[1000];
+	}
+	
+	public int findName(String gender, String name) {
+		int rank = -1;
+		
+		if(gender.equals("M")) {
+			for(int i = 0; i < 1000; i++) {
+				if(boyName[i].equals(name)) {
+					rank = i + 1;
+				}
+			}
+		}else {
+			for(int i = 0; i < 1000; i++) {
+				if(girlName.equals(name)) {
+					rank = i + 1;
+				}
+			}
+		}
+			
+		return rank;
+	}
+	
+	public void getFileInfo(String fileName) {
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader("babynamesranking" + fileName + ".txt"));
+			String line = reader.readLine();
+			int count = 0;
+			while(line != null) {							
+				StringTokenizer token;
+				
+				try {
+					if(line != null) {
+						token = new StringTokenizer(line);
+						
+						rank[count] = token.nextToken();
+						boyName[count] = token.nextToken();
+						boyNum[count] = token.nextToken();
+						girlName[count] = token.nextToken();
+						girlNum[count] = token.nextToken();
+					}
+						
+				}catch(Exception e) {
+					System.out.println("Exception: " + e);
+				}
+				
+				line = reader.readLine();
+				count++;
+			}
+			reader.close();
+			
+		}catch(IOException e) {
+			System.out.println("Exception: " + e);
+		}
+	}
+}
--- workshop6/test/TesterClass.java
+++ workshop6/test/TesterClass.java
@@ -2,10 +2,17 @@ package com.senecacollege.workshop6.test;
 
 import com.senecacollege.workshop6.task1.*;
 import com.senecacollege.workshop6.task2.*;
+import javafx.application.Application;
+
+import java.util.StringTokenizer;
 
 public class TesterClass {
 	public static void main(String[] args) {
-		GaltonBox galtonBox = new GaltonBox();
-		galtonBox.playGame();
+		//task1
+//		GaltonBox galtonBox = new GaltonBox();
+//		galtonBox.playGame();
+		
+		//task2
+		Application.launch(RankingPanel.class);
 	}
 }
